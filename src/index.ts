6ea8baac2de2606820a1a38b4945272c3e805af7
rambutan <pride.kraievskyi@gmail.com>
2024-04-03 15:34:43 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+import 'dotenv/config';
+
+
+const PORT = process.env.PORT || 8000;
+const app: Application = express();
+
+app.use(cors());
+
+const expressServer = app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+
+app.get("*", (_, res) => {
+	res.redirect(process.env.CLIENT_URL || "http://localhost:3000")
+})
