56d743729b23e53c4c543ce8d6b9d33ceec28d21
rambutan <pride.kraievskyi@gmail.com>
2024-04-03 15:44:08 +0300

    add prettier eslint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import cors from 'cors';
-import express, { Application } from 'express';
-import 'dotenv/config';
-
+import cors from "cors";
+import express, { Application } from "express";
+import "dotenv/config";
 
 const PORT = process.env.PORT || 8000;
 const app: Application = express();
@@ -9,9 +8,9 @@ const app: Application = express();
 app.use(cors());
 
 const expressServer = app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
 
 app.get("*", (_, res) => {
-	res.redirect(process.env.CLIENT_URL || "http://localhost:3000")
-})
+    res.redirect(process.env.CLIENT_URL || "http://localhost:3000");
+});
